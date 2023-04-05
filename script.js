188fd889d63e261dada59f6b33c6c704e2ee5f87
SantiagoCaso <santiagoignaciocaso@gmail.com>
2023-04-05 20:51:59 -0300

    Prueba apretar tecla 7

---
+++ script.js
@@ -0,0 +1,8 @@
+const boton7 = document.getElementById("7");
+const screen = document.getElementById("screen");
+
+boton7.addEventListener("click", consol7);
+
+function consol7() {
+  screen.innerHTML = "7";
+}
